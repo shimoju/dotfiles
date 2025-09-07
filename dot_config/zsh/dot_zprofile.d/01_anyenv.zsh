da466e78e97f725d2c31b362db9ef529bb9a0cca
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2025-09-07 10:06:37 +0900

    Add nodenv

--- dot_config/zsh/dot_zprofile.d/01_anyenv.zsh
+++ dot_config/zsh/dot_zprofile.d/01_anyenv.zsh
@@ -6,3 +6,7 @@ fi
 if [[ -x "$HOME/.anyenv/envs/rbenv/bin/rbenv" ]]; then
   FPATH="$HOME/.anyenv/envs/rbenv/completions:$FPATH"
 fi
+
+if [[ -x "$HOME/.anyenv/envs/nodenv/bin/nodenv" ]]; then
+  FPATH="$HOME/.anyenv/envs/nodenv/completions:$FPATH"
+fi
