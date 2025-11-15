c1193b97f440d8ed94a53ed60f5eae5dbdfdedd7
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2025-11-15 22:45:16 +0900

    早期リターンではなくifで書く

--- dot_config/zsh/dot_zshrc.d/neovim.zsh
+++ dot_config/zsh/dot_zshrc.d/neovim.zsh
@@ -1,8 +1,8 @@
 alias vi='vim'
 alias v='vim'
 
-(( ! $+commands[nvim] )) && return 1
-
-alias vim='nvim'
-export EDITOR='nvim'
-export VISUAL='nvim'
+if (( $+commands[nvim] )); then
+  alias vim='nvim'
+  export EDITOR='nvim'
+  export VISUAL='nvim'
+fi
