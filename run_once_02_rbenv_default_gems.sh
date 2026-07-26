45fd935e675a7d5257f143ee7668d7e771f9bff1
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-07-26 18:15:21 +0900

    ln -sfnを使わないと2回目以降の実行で意図通りにならない

--- run_once_02_rbenv_default_gems.sh
+++ run_once_02_rbenv_default_gems.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 if [[ -d "$HOME/.anyenv/envs/rbenv" ]]; then
-  ln -sf "$HOME/.config/rbenv-default-gems" "$HOME/.anyenv/envs/rbenv/default-gems"
+  ln -sfn "$HOME/.config/rbenv-default-gems" "$HOME/.anyenv/envs/rbenv/default-gems"
 fi
