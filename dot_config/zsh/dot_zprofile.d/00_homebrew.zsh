48044c3e2a7ba8b71d6426e32f5d0896aef75c8e
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-02 01:48:26 +0900

    HIST_EXPIRE_DUPS_FIRSTは実質意味のない設定になっているので消す&rustupのパス設定見直し

--- dot_config/zsh/dot_zprofile.d/00_homebrew.zsh
+++ dot_config/zsh/dot_zprofile.d/00_homebrew.zsh
@@ -1,4 +1,4 @@
 if [[ -x "/opt/homebrew/bin/brew" ]]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
-  path=("$(brew --prefix rustup)/bin" $path)
+  path=(/opt/homebrew/opt/rustup/bin(N) $path)
 fi
