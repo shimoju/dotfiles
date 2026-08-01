ed51d802019cc01f478aa7be855477eb26336cec
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-01 16:26:41 +0900

    preztoからantidoteに移行する
    preztoの設定は.zshrcに移し、プラグイン管理にantidoteを採用

--- dot_config/zsh/dot_zshrc.d/update.zsh
+++ dot_config/zsh/dot_zshrc.d/update.zsh
@@ -1,1 +1,1 @@
-alias update='brew update && brew upgrade --no-ask && brew cleanup && anyenv update && rustup update && zprezto-update'
+alias update='brew update && brew upgrade --no-ask && brew cleanup && anyenv update && rustup update && antidote update'
