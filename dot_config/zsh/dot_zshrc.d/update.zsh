0111065ab0ff8b3bfd6bdbf4d35bc642f0692635
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-02 12:14:07 +0900

    アップデートコマンドも関数化

--- dot_config/zsh/dot_zshrc.d/update.zsh
+++ dot_config/zsh/dot_zshrc.d/update.zsh
@@ -1,1 +1,15 @@
-alias update='brew update && brew upgrade --no-ask && brew cleanup && anyenv update && rustup update && antidote update'
+update() {
+  brew update && brew upgrade --no-ask
+  brew cleanup
+  anyenv update
+  rustup update
+  antidote update
+
+  if (( $+commands[claude] )); then
+    claude update
+  fi
+
+  if (( $+commands[codex] )); then
+    curl -fsSL https://chatgpt.com/codex/install.sh | CODEX_NON_INTERACTIVE=1 sh
+  fi
+}
