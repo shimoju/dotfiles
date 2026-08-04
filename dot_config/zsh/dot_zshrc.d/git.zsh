8bfd6b2d51208dd8e50e9b1a9fc71fc59e94179f
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-04 22:00:52 +0900

    git-gtrのシェル統合を有効にする
    https://github.com/coderabbitai/git-worktree-runner#git-gtr-go-branch

--- dot_config/zsh/dot_zshrc.d/git.zsh
+++ dot_config/zsh/dot_zshrc.d/git.zsh
@@ -1,3 +1,9 @@
 (( ! $+commands[git] )) && return 1
 
 alias g='git'
+
+if (( $+commands[git-gtr] )); then
+  _gtr_init="${XDG_CACHE_HOME:-$HOME/.cache}/gtr/init-gtr.zsh"
+  [[ -f "$_gtr_init" ]] || eval "$(git gtr init zsh)" || true
+  source "$_gtr_init" 2>/dev/null || true; unset _gtr_init
+fi
