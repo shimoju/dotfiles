8dc0f69be5c3006d852759f73a23963ef9f0c2da
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-05 23:25:12 +0900

    Configure Neovim plugins after loading

--- dot_config/nvim/lua/config/plugins.lua
+++ dot_config/nvim/lua/config/plugins.lua
@@ -1,7 +1,4 @@
 vim.pack.add({
   { src = "https://github.com/ibhagwan/fzf-lua" },
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
-}, { load = true })
-
-require("plugins.fzf")
-require("plugins.gitsigns")
+})
