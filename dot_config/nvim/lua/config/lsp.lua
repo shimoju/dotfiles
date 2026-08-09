f10d03c89e81a3ee5f86e857d44c5f5f4df376f1
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-09 22:56:02 +0900

    Add toggleable Neovim inlay hints

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -7,6 +7,15 @@ local formatter_by_filetype = {
   typescriptreact = "vtsls",
 }
 
+local vtsls_inlay_hints = {
+  parameterNames = { enabled = "literals" },
+  parameterTypes = { enabled = true },
+  variableTypes = { enabled = true },
+  propertyDeclarationTypes = { enabled = true },
+  functionLikeReturnTypes = { enabled = true },
+  enumMemberValues = { enabled = true },
+}
+
 local function format(bufnr)
   bufnr = bufnr == 0 and vim.api.nvim_get_current_buf() or bufnr
 
@@ -31,6 +40,22 @@ local function format(bufnr)
   })
 end
 
+local function toggle_inlay_hints()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local clients = vim.lsp.get_clients({
+    bufnr = bufnr,
+    method = "textDocument/inlayHint",
+  })
+  if #clients == 0 then
+    vim.notify("Inlay hints are not supported in this buffer", vim.log.levels.WARN)
+    return
+  end
+
+  local enabled = not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
+  vim.lsp.inlay_hint.enable(enabled, { bufnr = bufnr })
+  vim.notify("Inlay hints " .. (enabled and "enabled" or "disabled"))
+end
+
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = vim.api.nvim_create_augroup("config-lsp-format", { clear = true }),
   callback = function(args)
@@ -42,8 +67,17 @@ vim.keymap.set("n", "<leader>lf", function()
   format(0)
 end, { desc = "Format LSP buffer" })
 
+vim.keymap.set("n", "<leader>lh", toggle_inlay_hints, { desc = "Toggle LSP inlay hints" })
+
 vim.lsp.config("*", {
   capabilities = require("blink.cmp").get_lsp_capabilities(),
+})
+
+vim.lsp.config("vtsls", {
+  settings = {
+    typescript = { inlayHints = vtsls_inlay_hints },
+    javascript = { inlayHints = vim.deepcopy(vtsls_inlay_hints) },
+  },
 })
 
 vim.lsp.enable({ "ruby_lsp", "vtsls" })
