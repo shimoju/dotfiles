c543aa493e33c875d783b6c21a1deb13f641351f
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-10 03:33:29 +0900

    Open fzf results in Trouble

--- dot_config/nvim/after/plugin/fzf.lua
+++ dot_config/nvim/after/plugin/fzf.lua
@@ -1,6 +1,9 @@
 local fzf = require("fzf-lua")
+local fzf_config = require("fzf-lua.config")
+local trouble_actions = require("trouble.sources.fzf").actions
 
 fzf.register_ui_select()
+fzf_config.defaults.actions.files["ctrl-x"] = trouble_actions.open
 
 vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Live grep" })
