6e53fec9ae0b0a42ecfe9e469d9e71525d28e786
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-10 11:08:14 +0900

    Speed up Neovim theme startup

--- dot_config/nvim/after/plugin/catppuccin.lua
+++ dot_config/nvim/after/plugin/catppuccin.lua
@@ -1,6 +1,24 @@
 require("catppuccin").setup({
   flavour = "mocha",
-  auto_integrations = true,
+  default_integrations = false,
+  integrations = {
+    blink_cmp = { enabled = true, style = "bordered" },
+    dap = true,
+    dap_ui = true,
+    fidget = true,
+    fzf = true,
+    gitsigns = true,
+    indent_blankline = {
+      enabled = true,
+      scope_color = "",
+      colored_indent_levels = false,
+    },
+    lsp_trouble = true,
+    neotest = true,
+    nvim_surround = true,
+    nvimtree = true,
+    which_key = true,
+  },
 })
 
 vim.cmd.colorscheme("catppuccin-mocha")
