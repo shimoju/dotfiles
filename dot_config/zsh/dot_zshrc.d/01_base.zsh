e0ecdb89131dbd6afc9ad0564c370b13d2cf930c
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-12 01:21:39 +0900

    fzfをbaseに移動

--- dot_config/zsh/dot_zshrc.d/01_base.zsh
+++ dot_config/zsh/dot_zshrc.d/01_base.zsh
@@ -24,3 +24,24 @@ else
   alias la='ls -A'
   alias ll='ls -lhA'
 fi
+
+# fzf
+if (( $+commands[fzf] )); then
+  source <(fzf --zsh)
+
+  bindkey -M emacs $'\x1e' fzf-cd-widget
+
+  f() {
+    local dir
+    dir="$(ghq list --full-path | fzf --height "${FZF_TMUX_HEIGHT:-40%}" --min-height 20+ --bind=ctrl-z:ignore --reverse)" || return
+    [[ -n "$dir" ]] || return
+    builtin cd -- "$dir"
+  }
+
+  _ghq_fzf_cd_widget() {
+    f
+    zle reset-prompt
+  }
+  zle -N _ghq_fzf_cd_widget
+  bindkey -M emacs '^O' _ghq_fzf_cd_widget
+fi
