46931d9fbc3cd7dd0783987081c23ae0db978499
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-11 10:17:39 +0900

    ウインドウ周りの設定
    - タブがひとつのみのときはタブバーを非表示にする
    - 起動時のウインドウサイズを指定
    - タイトルバーを非表示にする（リサイズは可能）

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,6 +5,11 @@ config.font = wezterm.font 'Moralerspace Argon HW'
 config.font_size = 14
 config.color_scheme = 'Catppuccin Mocha'
 
+config.hide_tab_bar_if_only_one_tab = true
+config.initial_cols = 240
+config.initial_rows = 80
+config.window_decorations = 'RESIZE'
+
 config.use_ime = true
 
 return config
