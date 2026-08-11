c10d2a21a43c2e74a43a796960d93c3435c70265
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-11 11:18:08 +0900

    yaziのshell wrapper設定を入れる
    https://yazi-rs.github.io/docs/quick-start/#shell-wrapper

---
+++ dot_config/zsh/dot_zshrc.d/yazi.zsh
@@ -0,0 +1,9 @@
+(( ! $+commands[yazi] )) && return 1
+
+function y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  command yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd < "$tmp"
+  [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+  command rm -f -- "$tmp"
+}
