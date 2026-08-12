87bae42f2509f6818cc20b4819aa7bd09d5b459c
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-12 22:18:49 +0900

    starshipの代わりにPureを導入
    必要十分な機能があり、gitのステータス表示が非同期で更新されるため高速
    git stash表示も非同期でチェックされるため有効化する
    https://github.com/sindresorhus/pure

--- dot_config/zsh/dot_zshrc.d/02_prompt.zsh
+++ dot_config/zsh/dot_zshrc.d/02_prompt.zsh
@@ -1,4 +1,12 @@
-# starship
-if (( $+commands[starship] )); then
-  eval "$(starship init zsh)"
-fi
+# Pure
+fpath+=("${ZDOTDIR:-$HOME}/pure")
+
+autoload -Uz promptinit
+promptinit
+
+zstyle ':prompt:pure:environment:virtualenv' show no
+zstyle ':prompt:pure:environment:nix-shell' show no
+zstyle ':prompt:pure:git:dirty' detailed yes
+zstyle ':prompt:pure:git:stash' show yes
+
+prompt pure
