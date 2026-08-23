0ae07fc9f78e47cfd22efaa77eb350c50cae7cdc
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-23 19:02:55 +0900

    ripgrepのマッチ表示色を黒文字・黄色背景に設定
    grepの設定と統一する

--- dot_config/zsh/dot_zshrc.d/00_base.zsh
+++ dot_config/zsh/dot_zshrc.d/00_base.zsh
@@ -54,6 +54,9 @@ else
   alias c='cat'
 fi
 
+# ripgrep
+export RIPGREP_CONFIG_PATH="${XDG_CONFIG_HOME:-$HOME/.config}/ripgrep/config"
+
 # zoxide
 if (( $+commands[zoxide] )); then
   eval "$(zoxide init zsh)"
