fecd5e9f5cd30c3f17a24b1de8939edac74eb4a9
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-29 14:15:09 +0900

    lsの実装判定を簡潔にする

--- dot_config/zsh/dot_zshrc.d/00_base.zsh
+++ dot_config/zsh/dot_zshrc.d/00_base.zsh
@@ -21,19 +21,14 @@ fi
 
 # Builtin commands
 # macOS /bin/ls has no --version, so avoid spawning it only to confirm that.
-if [[ "$OSTYPE" == darwin* && "$commands[ls]" == /bin/ls ]]; then
-  _ls_version=
-else
-  _ls_version="$(command ls --version 2>&1)"
-fi
-if [[ ${(@M)${(f)_ls_version}:#*(GNU|lsd|uutils) *} ]]; then
+if ! [[ "$OSTYPE" == darwin* && "$commands[ls]" == /bin/ls ]] &&
+  [[ "$(command ls --version 2>&1)" == *(GNU|lsd|uutils)\ * ]]; then
   export LS_COLORS=${LS_COLORS:-'di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'}
   alias ls='ls --color=auto'
 else
   export LSCOLORS=${LSCOLORS:-'exfxcxdxbxGxDxabagacad'}
   alias ls='ls -G'
 fi
-unset _ls_version
 
 export GREP_COLOR='1;30;43'
 export GREP_COLORS="mt=$GREP_COLOR"
