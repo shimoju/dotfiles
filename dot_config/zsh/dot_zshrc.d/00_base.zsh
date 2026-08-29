d0347e0a6b316600a11c4458d7421127207ebb91
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2026-08-29 10:24:46 +0900

    macOS標準lsのversion確認を省略する

--- dot_config/zsh/dot_zshrc.d/00_base.zsh
+++ dot_config/zsh/dot_zshrc.d/00_base.zsh
@@ -20,7 +20,12 @@ if (( $+commands[mise] )); then
 fi
 
 # Builtin commands
-_ls_version="$(command ls --version 2>&1)"
+# macOS /bin/ls has no --version, so avoid spawning it only to confirm that.
+if [[ "$OSTYPE" == darwin* && "$commands[ls]" == /bin/ls ]]; then
+  _ls_version=
+else
+  _ls_version="$(command ls --version 2>&1)"
+fi
 if [[ ${(@M)${(f)_ls_version}:#*(GNU|lsd|uutils) *} ]]; then
   export LS_COLORS=${LS_COLORS:-'di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'}
   alias ls='ls --color=auto'
